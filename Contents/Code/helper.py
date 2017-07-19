a45f596eac23a041262749033d6fe743e9b15d98
Joshua <joshuaavalon@gmail.com>
2017-07-19 23:10:05 +0800

    Add Xml support

--- Contents/Code/helper.py
+++ Contents/Code/helper.py
@@ -1,4 +1,4 @@
-import os
+from os.path import dirname, join, splitext, exists, basename
 import re
 
 from log import *
@@ -23,13 +23,13 @@ def get_show_directory(media):
             file_path = media.seasons[season].episodes[episode].items[0].parts[0].file
     if file_path is None:
         return None
-    season_directory = os.path.dirname(file_path)
-    return os.path.dirname(season_directory)
+    season_directory = dirname(file_path)
+    return dirname(season_directory)
 
 
 def get_movie_directory(media):
     file_path = get_movie_path(media)
-    return os.path.dirname(file_path)
+    return dirname(file_path)
 
 
 def get_movie_path(media):
@@ -44,7 +44,7 @@ def get_show_nfo_path(show_directory):
     :type show_directory: str
     :return: tvshow.nfo path
     """
-    return os.path.join(show_directory, "tvshow.nfo")
+    return select_exist(join(show_directory, "tvshow.xml"), join(show_directory, "tvshow.nfo"))
 
 
 def get_show_title(media):
@@ -60,7 +60,7 @@ def get_show_nfo(media):
     if show_directory is None:
         return None
     nfo_path = get_show_nfo_path(show_directory)
-    if os.path.exists(nfo_path):
+    if nfo_path is not None:
         xml_str = Core.storage.load(nfo_path)
         return XML.ElementFromString(xml_str)
     else:
@@ -68,30 +68,22 @@ def get_show_nfo(media):
 
 
 def get_episode_nfo(media, season, episode):
+    standard_xml_path, fallback_xml_path = guess_episode_asset(media, season, episode, "xml")
     standard_nfo_path, fallback_nfo_path = guess_episode_asset(media, season, episode, "nfo")
-    nfo_path = None
-    if os.path.exists(standard_nfo_path):
-        nfo_path = standard_nfo_path
-    else:
-        if os.path.exists(fallback_nfo_path):
-            nfo_path = fallback_nfo_path
-    if nfo_path is not None:
-        xml_str = Core.storage.load(nfo_path)
+    file_path = select_exist(standard_xml_path, standard_nfo_path, fallback_xml_path, fallback_nfo_path)
+    if file_path is not None:
+        xml_str = Core.storage.load(file_path)
         return XML.ElementFromString(xml_str)
     else:
         return None
 
 
 def get_movie_nfo(media):
+    standard_xml_path, fallback_xml_path = guess_movie_asset(media, "xml")
     standard_nfo_path, fallback_nfo_path = guess_movie_asset(media, "nfo")
-    nfo_path = None
-    if os.path.exists(standard_nfo_path):
-        nfo_path = standard_nfo_path
-    else:
-        if os.path.exists(fallback_nfo_path):
-            nfo_path = fallback_nfo_path
-    if nfo_path is not None:
-        xml_str = Core.storage.load(nfo_path)
+    file_path = select_exist(standard_xml_path, standard_nfo_path, fallback_xml_path, fallback_nfo_path)
+    if file_path is not None:
+        xml_str = Core.storage.load(file_path)
         return XML.ElementFromString(xml_str)
     else:
         return None
@@ -105,11 +97,11 @@ def debug_print_object(obj):
 
 def guess_episode_asset(media, season, episode, ext):
     file_path = media.seasons[season].episodes[episode].items[0].parts[0].file
-    file_directory = os.path.dirname(file_path)
+    file_directory = dirname(file_path)
     title = guess_episode_name(file_path)
     standard_name = "%s - s%se%s.%s" % (title, season.zfill(2), episode.zfill(2), ext)
-    standard_path = os.path.join(file_directory, standard_name)
-    fallback_path = "%s.%s" % (os.path.splitext(file_path)[0], ext)
+    standard_path = join(file_directory, standard_name)
+    fallback_path = "%s.%s" % (splitext(file_path)[0], ext)
     return standard_path, fallback_path
 
 
@@ -118,21 +110,21 @@ def guess_movie_asset(media, ext):
     file_directory = get_movie_directory(media)
     name, year = guess_movie_name_year(file_path)
     standard_name = "%s (%s).%s" % (name, year, ext)
-    standard_path = os.path.join(file_directory, standard_name)
-    fallback_path = "%s.%s" % (os.path.splitext(file_path)[0], ext)
+    standard_path = join(file_directory, standard_name)
+    fallback_path = "%s.%s" % (splitext(file_path)[0], ext)
     return standard_path, fallback_path
 
 
 def guess_episode_name(file_path):
-    file_name = os.path.basename(file_path)
-    file_name_without_ext = os.path.splitext(file_name)[0]
+    file_name = basename(file_path)
+    file_name_without_ext = splitext(file_name)[0]
     result = re.search(EPISODE_REGEX, file_name_without_ext)
     return result.group(1)
 
 
 def guess_movie_name_year(file_path):
-    file_name = os.path.basename(file_path)
-    file_name_without_ext = os.path.splitext(file_name)[0]
+    file_name = basename(file_path)
+    file_name_without_ext = splitext(file_name)[0]
     result = re.search(MOVIE_REGEX, file_name_without_ext)
     return result.group(1), result.group(2)
 
@@ -148,7 +140,7 @@ def get_episode_thumb(media, season, episode):
     paths.extend(standard_thumb_paths)
     paths.extend(fallback_thumb_paths)
     for path in paths:
-        if os.path.exists(path):
+        if exists(path):
             thumb = Core.storage.load(path)
             return path, Proxy.Media(thumb)
     return None
@@ -156,10 +148,17 @@ def get_episode_thumb(media, season, episode):
 
 def get_actor_thumb(name):
     actor_directory = Prefs["ActorsDirectory"]
-    if not actor_directory or not os.path.exists(actor_directory):
+    if not actor_directory or not exists(actor_directory):
         return ""
     for ext in IMAGE_EXTS:
-        image_path = os.path.join(actor_directory, "%s.%s" % (name, ext))
-        if os.path.exists(image_path):
+        image_path = join(actor_directory, "%s.%s" % (name, ext))
+        if exists(image_path):
             return image_path
     return ""
+
+
+def select_exist(*args):
+    for path in enumerate(args):
+        if exists(path):
+            return path
+    return None
